09c716bfb719840c4988be524fdebebb8b0192df
athakur7 <anandthakur178@gmail.com>
2023-09-25 09:51:23 +0000

    simple s3 bucket #11

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 terraform {
   required_providers {
     random = {
-      source = "hashicorp/random"
+      source  = "hashicorp/random"
       version = "3.5.1"
     }
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "5.17.0"
     }
   }
@@ -14,13 +14,18 @@ terraform {
 provider "random" {
   # Configuration options
 }
-
-resource "random_string" "bucket_name" {
-  length   = 6
-  special  = false
-  upper = false
+provider "aws" {
+  # Configuration options
 }
 
+# https://registry.terraform.io/providers/hashicorp/random/latest/docs/resources/string
+resource "random_string" "bucket_name" {
+  length  = 6
+  special = false
+  upper   = false
+}
+
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket
 resource "aws_s3_bucket" "aws_bucket_name" {
   bucket = "anand-s3-terraform-bucket-${random_string.bucket_name.result}"
 }
