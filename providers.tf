813780311e1d2b833c50240a15eb6d2293b02c14
athakur7 <anandthakur178@gmail.com>
2023-09-26 09:41:47 +0000

    restructure root module  and add tagging to s3 bucket #23

---
+++ providers.tf
@@ -0,0 +1,25 @@
+terraform {
+#   cloud {
+#     organization = "TerraFusion"
+#     workspaces {
+#       name = "terra-house-1"
+#     }
+#   }
+  required_providers {
+    random = {
+      source  = "hashicorp/random"
+      version = "3.5.1"
+    }
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.17.0"
+    }
+  }
+}
+
+provider "random" {
+  # Configuration options
+}
+provider "aws" {
+  # Configuration options
+}
